6384a62c76ba47815893050e440b88b1192b03c8
David Cramer <dcramer@gmail.com>
2011-06-13 14:45:53 -0700

    We dont actually need BackendConnectionProxy

--- nydus/contrib/django/backend/base.py
+++ nydus/contrib/django/backend/base.py
@@ -5,27 +5,13 @@ from django.core.exceptions import ImproperlyConfigured
 
 from nydus.db import connections
 
-class BackendConnectionProxy(object):
-    """
-    Proxies a ``BaseConnection`` with an additional ``alias``
-    attribute.
-    """
-    def __init__(self, backend, alias):
-        self.backend = backend
-        self.alias = alias
-    
-    def __getattr__(self, attr):
-        if attr in self.__dict__:
-            return self.__dict__[attr]
-        else:
-            return getattr(self.backend, attr)
-
 class DatabaseWrapper(object):
     def __init__(self, settings_dict, alias=DEFAULT_DB_ALIAS):
         self.settings_dict = settings_dict
+        self.alias = alias
         try:
             nydus_alias = settings_dict['NAME']
-            self.backend = BackendConnectionProxy(connections[nydus_alias], alias)
+            self.backend = connections[nydus_alias]
         except KeyError, e:
             # TODO: give a better exception
             raise ImproperlyConfigured(e)
