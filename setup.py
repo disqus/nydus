45309276bba437bc24ec28de298282fee727a9a1
David Cramer <dcramer@gmail.com>
2012-01-18 10:34:36 -0800

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -16,11 +16,11 @@ install_requires = [
 
 setup(
     name='nydus',
-    version='0.1.2',
+    version='0.2.0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/disqus/nydus',
-    description = 'Connection utilities',
+    description='Connection utilities',
     packages=find_packages(exclude=('tests',)),
     zip_safe=False,
     install_requires=install_requires,
