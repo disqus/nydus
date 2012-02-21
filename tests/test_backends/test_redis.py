67f6bb07b197abdc8eab6e8bd14bf91ae9230587
George Courtsunis <gjcourt@gmail.com>
2012-02-21 13:27:32 -0800

    Add pipeline_map command to the Cluster class

--- tests/test_backends/test_redis.py
+++ tests/test_backends/test_redis.py
@@ -9,17 +9,18 @@ tests.test_backends.test_redis
 from .. import BaseTest
 
 from nydus.db.backends.redis import Redis
-from nydus.db import Cluster
+from nydus.db import Cluster, create_cluster
 import redis
 
 class RedisTest(BaseTest):
+
     def setUp(self):
         self.redis = Redis(num=0, db_num=1)
         self.redis.flushdb()
 
     def test_proxy(self):
         self.assertEquals(self.redis.incr('RedisTest_proxy'), 1)
-    
+
     def test_with_cluster(self):
         p = Cluster(
             hosts={0: self.redis},
@@ -31,3 +32,22 @@ class RedisTest(BaseTest):
 
     def test_provides_identifier(self):
         self.assertEquals(self.redis.identifier, str(self.redis.identifier))
+
+    def test_pipelined_map(self):
+        redis = create_cluster({
+            'engine': 'nydus.db.backends.redis.Redis',
+            'router': 'nydus.db.routers.redis.PartitionRouter',
+            'hosts': {
+                0: {'db': 5},
+                1: {'db': 6},
+                2: {'db': 7},
+                3: {'db': 8},
+                4: {'db': 9},
+            }
+        })
+        chars = ('a', 'b', 'c', 'd', 'e', 'f')
+        with redis.pipelined_map() as conn:
+            [conn.set(c, i) for i, c in enumerate(chars)]
+            res = [conn.get(c) for c in chars]
+        self.assertEqual(range(len(chars)), [int(r._wrapped) for r in res])
+
