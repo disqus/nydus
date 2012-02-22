107bd7b51b79dab6ec635c8011b4962518aaca88
David Cramer <dcramer@gmail.com>
2012-02-21 20:55:40 -0800

    Add precise test for Redis client behavior in pipeline

--- tests/nydus/db/backends/redis/tests.py
+++ tests/nydus/db/backends/redis/tests.py
@@ -4,6 +4,7 @@ from tests import BaseTest
 
 from nydus.db.backends.redis import Redis
 from nydus.db.base import Cluster, create_cluster
+import mock
 import redis
 
 
@@ -46,3 +47,33 @@ class RedisTest(BaseTest):
             res = [conn.get(c) for c in chars]
         self.assertEqual(range(len(chars)), [int(r._wrapped) for r in res])
 
+    @mock.patch('nydus.db.backends.redis.RedisClient')
+    def test_map_does_pipeline(self, RedisClient):
+        redis = create_cluster({
+            'engine': 'nydus.db.backends.redis.Redis',
+            'router': 'nydus.db.routers.redis.PartitionRouter',
+            'hosts': {
+                0: {'db': 0},
+                1: {'db': 1},
+            }
+        })
+        chars = ('a', 'b')
+        with redis.map() as conn:
+            [conn.set(c, i) for i, c in enumerate(chars)]
+
+        self.assertEquals(RedisClient.call_count, 2)
+        RedisClient.assert_any_call(host='localhost', port=6379, db=0, socket_timeout=None)
+        RedisClient.assert_any_call(host='localhost', port=6379, db=1, socket_timeout=None)
+
+        # ensure this was actually called through the pipeline
+        self.assertFalse(RedisClient().set.called)
+
+        self.assertEquals(RedisClient().pipeline.call_count, 2)
+        RedisClient().pipeline.assert_called_with()
+
+        self.assertEquals(RedisClient().pipeline().set.call_count, 2)
+        RedisClient().pipeline().set.assert_any_call('a', 0)
+        RedisClient().pipeline().set.assert_any_call('b', 1)
+
+        self.assertEquals(RedisClient().pipeline().execute.call_count, 2)
+        RedisClient().pipeline().execute.assert_called_with()
