c93257d0a30e120051eac6a9ca6501e27069cc2b
David Cramer <dcramer@gmail.com>
2012-10-12 10:48:12 -0700

    misaligned headers

--- nydus/db/backends/thoonk.py
+++ nydus/db/backends/thoonk.py
@@ -1,6 +1,6 @@
 """
 nydus.db.backends.thoonk
-~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~
 
 :copyright: (c) 2011-2012 DISQUS.
 :license: Apache License 2.0, see LICENSE for more details.
--- nydus/db/routers/keyvalue.py
+++ nydus/db/routers/keyvalue.py
@@ -1,6 +1,6 @@
 """
 nydus.db.routers.keyvalue
-~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~
 
 :copyright: (c) 2011-2012 DISQUS.
 :license: Apache License 2.0, see LICENSE for more details.
