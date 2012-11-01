2c392fff662d59ad739171e1a38696ebfea89620
David Cramer <dcramer@gmail.com>
2012-11-01 13:39:21 -0700

    0.10.3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ install_requires = [
 
 setup(
     name='nydus',
-    version='0.10.2',
+    version='0.10.3',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/disqus/nydus',
